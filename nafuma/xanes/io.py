67ba03dc0c70d194348135cb8b62a17783b2f3f8
rasmusvt <r.v.thogersen@kjemi.uio.no>
2022-04-08 14:30:05 +0200

    Fix lint problem in xanes.io.py

--- nafuma/xanes/io.py
+++ nafuma/xanes/io.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import os
 import numpy as np
 
-def split_xanes_scan(root, destination=None, replace=False):
+def split_xanes_scan(filename, destination=None, replace=False):
     #root is the path to the beamtime-folder
     #destination should be the path to the processed data
     
