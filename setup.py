c6b5dc4627929c58753ef885cedf4c864a2034cf
rasmusvt <r.v.thogersen@kjemi.uio.no>
2022-07-07 15:15:07 +0200

    Update with new version number

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='nafuma',
-      version='0.2',
+      version='0.3',
       description='Analysis tools for inorganic materials chemistry at the NAFUMA-group at the University of Oslo',
       url='https://github.com/rasmusthog/nafuma',
       author='Rasmus Vester Thøgersen, Halvor Høen Hval',
