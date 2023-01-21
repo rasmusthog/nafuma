d32b4b7436007313b03cbf6d2b863b96ac382100
halvorhv <h.h.hval@kjemi.uio.no>
2023-01-21 02:26:44 +0100

    Adding excluded regions to background subtraction

--- nafuma/xrd/analytics.py
+++ nafuma/xrd/analytics.py
@@ -9,19 +9,17 @@ import os
 
 def background_subtracted_peak(data,options):
 
-
-
     default_options = {
     'plot_before_and_after':  False,
     'background_shoulder_left': 0.1, #picking out how far from the peak edge on the left side the background will be extracted from
     'background_shoulder_right': 0.1, #picking out how far from the peak edge on the left side the background will be extracted from
+    'background_region': False, #Provide an interval [x1,x2]
+    'background_excluded_region': None,
     'peak_interval': False, #Provide an interval [x1,x2]
     'save_dir': 'background_subtracted_peak',
     'background_poly_degree': 1,
     'plot_all_background_fits': False
     }
-
-
 
     diffractogram, wavelength = xrd.io.read_xy(data=data,options=options)   
 
@@ -52,9 +50,9 @@ def background_subtracted_peak(data,options):
     peak_y=[] #for each peak of interest, I hereby fill in the y-values of the peak 
     #background_points_for_normalization=[]
     #x_range=[]
-
     for i, twotheta in enumerate(diffractogram["2th"]): #using the background start and end points to define the regions of interest
-        if options['peak_interval'][0]-options['background_shoulder_left'] < twotheta and twotheta < options['peak_interval'][1]+options['background_shoulder_right']:
+        #if options['peak_interval'][0]-options['background_shoulder_left'] < twotheta and twotheta < options['peak_interval'][1]+options['background_shoulder_right']:
+        if options['background_region'][0] < twotheta and twotheta < options['background_region'][1]:
             background_full_x.append(twotheta)
             data_full_y.append(diffractogram['I'][i])
             if  twotheta < options['peak_interval'][0]:
@@ -63,28 +61,32 @@ def background_subtracted_peak(data,options):
             elif twotheta < options['peak_interval'][1]:
                 peak_x.append(twotheta)
                 peak_y.append(diffractogram["I"][i])
-            elif twotheta < options['peak_interval'][1]+options['background_shoulder_right']:
+            elif twotheta < options['background_region'][1]:
                 background_right_shoulder_x.append(twotheta)
                 background_right_shoulder_y.append(diffractogram["I"][i])
 
     background_shoulders_x=np.concatenate((background_left_shoulder_x, background_right_shoulder_x))
     background_shoulders_y=np.concatenate((background_left_shoulder_y, background_right_shoulder_y))  
-    #if backgroundstart[j] < values and values < peakstart[j]: #only picking out the data points between backgroun_start and peak_start, as these will be used to normalize the background
-             #   background_points_for_normalization.append(df["intensity_tot"][i])
-    #background_points_x.append(background_x)
-    #background_normalization_factor = np.average(background_points_for_normalization)
-    #normalized_background_y=background_y/background_normalization_factor
-    #print('Normalization factor for '+str(peak)+' is '+str(background_normalization_factor))
-    #normalized_background_points_y.append(normalized_background_y)
-    #print(normalized_background_points_y)
-            #x_range.append(values)
+    #print(options['background_excluded_region'])
+    
+    #if not options['background_excluded_region']) == 0:# is not False:
+    for region in options['background_excluded_region']:
+        #print(region)
+        for i, xval in enumerate(background_shoulders_x):
+            if region[0] < xval < region[1]:
+                #because these are numpy arrays I am going via normal arrays to remove the values within the excluded regions:
+                background_x_to_list=background_shoulders_x.tolist()
+                background_x_to_list.remove(xval)
+                background_shoulders_x = np.asarray(background_x_to_list)
+                
+                background_y_to_list=background_shoulders_y.tolist()
+                background_y_to_list.pop(i)
+                background_shoulders_y = np.asarray(background_y_to_list)
+
+                #background_shoulders_y.pop(i)
+       # if region[0] 
 
 
-            #test
-
-
-    #print(background_shoulders_x)
-    #print(background_shoulders_y)
     d = np.polyfit(background_shoulders_x, background_shoulders_y,options['background_poly_degree'])
     function_background = np.poly1d(d) #Using the values of the background to make a backgroudn (2. deg polynomial)
     
@@ -106,7 +108,8 @@ def background_subtracted_peak(data,options):
         
         ax.set_ylim(min(df_peak['I_org'])*0.9,max(df_peak['I_org'])*1.1)
         
-        ax.set_xlim(options['peak_interval'][0]-2*(options['background_shoulder_left']+options['background_shoulder_right']),options['peak_interval'][1]+2*(options['background_shoulder_left']+options['background_shoulder_right']))
+        #ax.set_xlim(options['peak_interval'][0]-2*(options['background_shoulder_left']+options['background_shoulder_right']),options['peak_interval'][1]+2*(options['background_shoulder_left']+options['background_shoulder_right']))
+        ax.set_xlim(options['background_region'][0]*0.95,options['background_region'][1]*1.05)
         ax.set_title(filename)
         diffractogram.plot(x="2th",y="I",ax=ax)
         
@@ -243,8 +246,8 @@ def find_fwhm_of_peak(x,y,start_values,options):
     #plt.axvline(x = options['peak_interval'][1])
     
     # plt.scatter(x=x,y=y)
-        ax.plot(x_fit,y_fit)#,col='b')
-    #print(x)
+
+        ax.plot(x_fit,y_fit)
     return parameters, errors
 
 def read_timestamps(data,options):
@@ -265,7 +268,6 @@ def read_timestamps(data,options):
     for i,time in enumerate(df_time['Time']):
         time_string=df_time['Time'].iloc[i]
         date_string=df_time['Date'].iloc[i]
-        #print(date_string)
         time_stamp=datetime.datetime(
         year        =int(date_string.split('-')[0]),
         month       =int(date_string.split('-')[1]),
@@ -276,12 +278,10 @@ def read_timestamps(data,options):
         microsecond =int(time_string.split(':')[2].split('.')[1])*10**options['decimals_for_seconds'])
         
         time_stamp_list.append(time_stamp)
-    #print(df_time.columns)
     df_time_new=pd.DataFrame(df_time["#!File_name"].values,columns=["Filename"])
     #df_time_new=pd.DataFrame(time_stamp_list,columns=["DateTime"])
     df_time_new['DateTime'] = time_stamp_list#df_time["#!File_name"].values
     #df_time_new=pd.concat([df_time_new1, df_time_new2], axis=1)
-    #print(df_time["#!File_name"])
     
     
     #fixing scan time:
@@ -299,12 +299,10 @@ def read_timestamps(data,options):
     for time in df_time_new["DateTime"]:
         relative_time=time-first_timestamp
         relative_time_in_h=relative_time.days * 24 + relative_time.seconds/3600 + relative_time.microseconds/3600000000
-        #print(relative_time_in_h)
         relative_time_list_h.append(relative_time_in_h) 
     
     df_time_new["Relative_time"]=relative_time_list_h
-    #print(df_time_new)
-    #df_time_new["Filename"]=df_time["#!Filename"]
+
     return first_timestamp_corrected, df_time_new
 
     
