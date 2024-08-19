f11b1a10d5b03b80f0d0c845d2103d8664db597b
halvorhv <h.h.hval@kjemi.uio.no>
2024-08-19 20:50:17 +0200

    copied some things from rasmus' branch to make xanes analysis work

--- nafuma/auxillary.py
+++ nafuma/auxillary.py
@@ -166,7 +166,6 @@ def swap_values(options: dict, key1, key2):
     return options
 
 
-
 def find_neighbours(value, df, colname, start=0, end=-1):
     ''' Finds closest match to a given value in colname of df. If there is an exact match, returns index of this value. Else, it returns the nearest neighbors (upper and lower)'''
     
@@ -174,12 +173,24 @@ def find_neighbours(value, df, colname, start=0, end=-1):
     
     exactmatch = df[df[colname] == value]
     if not exactmatch.empty:
-        return exactmatch.index
+        return exactmatch.index.values[0]
     else:
         lower_df = df[df[colname] < value][colname]
         upper_df = df[df[colname] > value][colname]
 
-        lowerneighbour_ind = lower_df.idxmax()
-        upperneighbour_ind = upper_df.idxmin()
+
+        if not lower_df.empty:
+            lowerneighbour_ind = lower_df.idxmax()
+        else:
+            lowerneighbour_ind = np.nan
+        
+        if not upper_df.empty:
+            upperneighbour_ind = upper_df.idxmin()
+        else:
+            upperneighbour_ind = np.nan
 
         return [lowerneighbour_ind, upperneighbour_ind] 
+
+def isnan(value):
+
+    return value!=value
